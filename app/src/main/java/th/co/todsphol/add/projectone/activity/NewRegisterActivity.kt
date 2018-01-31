ec888a3bc3f88031a7e8229b62ef3f2105ada481
Todsphol <theaddza11@gmail.com>
2018-01-31 19:43:37 +0700

    Add new Activity Selected Image

--- app/src/main/java/th/co/todsphol/add/projectone/activity/NewRegisterActivity.kt
+++ app/src/main/java/th/co/todsphol/add/projectone/activity/NewRegisterActivity.kt
@@ -1,13 +1,8 @@
 package th.co.todsphol.add.projectone.activity
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.app.ProgressDialog
 import android.content.Intent
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
@@ -17,25 +12,17 @@ import butterknife.ButterKnife
 import com.jaredrummler.materialspinner.MaterialSpinner
 import th.co.todsphol.add.projectone.R
 import android.support.design.widget.Snackbar
-import android.support.v4.provider.FontsContractCompat
-import android.view.View
 import android.widget.*
 import butterknife.OnClick
-import com.bumptech.glide.Glide
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import kotlinx.android.synthetic.main.activity_new__register.*
 import th.co.todsphol.add.projectone.PhoneNumberWatcher
-import java.io.IOException
-import java.util.*
 
 
 @Suppress("DEPRECATION")
-class NewRegisterActivity : AppCompatActivity(), View.OnClickListener {
+class NewRegisterActivity : AppCompatActivity(){
 
     private var baseR = FirebaseDatabase.getInstance().reference
     private var dataName = baseR.child("User").child("user1").child("DATA_PERS")
@@ -43,9 +30,6 @@ class NewRegisterActivity : AppCompatActivity(), View.OnClickListener {
     private var dataStatus = baseR.child("User").child("user1").child("STATUS")
     private var dataREG = baseR.child("User").child("user1").child("DATA_REG")
     private var dataLocation = baseR.child("User").child("user1").child("DATA_LOCATION")
-    private var filePath : Uri? = null
-    var storage : FirebaseStorage? = null
-    var storageReference : StorageReference? = null
     @BindView(R.id.toolbar) lateinit var toolBar: Toolbar
     @BindView(R.id.tv_toolbar_title) lateinit var titleToolbar: TextView
     @BindView(R.id.edt_first_name) lateinit var edtFirstName: EditText
@@ -57,11 +41,6 @@ class NewRegisterActivity : AppCompatActivity(), View.OnClickListener {
     @BindView(R.id.edt_phone_number) lateinit var edtPhoneNumber: EditText
     @BindView(R.id.edt_password) lateinit var edtPassword: EditText
     @BindView(R.id.edt_confirm_password) lateinit var edtConfirmPassword: EditText
-    @BindView(R.id.btn_select) lateinit var btnSelected : Button
-    @BindView(R.id.imageView) lateinit var ivSelected : ImageView
-    @BindView(R.id.btn_register) lateinit var btnRegister : Button
-    var REQUEST_CODE = 1
-    val PICK_IMAGE_REQUEST = 1234
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,86 +50,7 @@ class NewRegisterActivity : AppCompatActivity(), View.OnClickListener {
         spinnerBrand()
 
         edtPhoneNumber.addTextChangedListener(PhoneNumberWatcher(edtPhoneNumber))
-//        btnSelected.setOnClickListener {
-//            val permissionCheck = ContextCompat.checkSelfPermission(this,
-//                    android.Manifest.permission.READ_EXTERNAL_STORAGE)
-//            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
-//                startGallery()
-//            } else {
-//                ActivityCompat.requestPermissions(this,
-//                        Array(5, { android.Manifest.permission.READ_EXTERNAL_STORAGE }),
-//                        2000)
-//            }
-//        }
-//        val intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-//            startActivityForResult(intent, REQUEST_CODE)
-        storage = FirebaseStorage.getInstance()
-        storageReference = storage?.reference
-        btnSelected.setOnClickListener(this)
-        btnRegister.setOnClickListener(this)
     }
-    override fun onClick(p0: View?) {
-        if (p0 == btnSelected) {
-            showFileChooser()
-        }else if(p0 == btnRegister) {
-            uploadFile()
-        }
-    }
-
-    private fun uploadFile() {
-        if (filePath != null) {
-            val progressDialog = ProgressDialog(this)
-            progressDialog.setTitle("Uploading...")
-            progressDialog.show()
-
-            val imageRef = storageReference!!.child("images/" + UUID.randomUUID().toString())
-            imageRef.putFile(filePath!!)
-                    .addOnSuccessListener {
-                        progressDialog.dismiss()
-                        Toast.makeText(applicationContext, "File Uploaded", Toast.LENGTH_SHORT).show()
-
-                    }
-                    .addOnFailureListener {
-                        progressDialog.dismiss()
-                        Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
-                    }
-                    .addOnProgressListener {taskSnapShot ->
-                        val progress = 100.0 * taskSnapShot.bytesTransferred/taskSnapShot.totalByteCount
-                        progressDialog.setMessage("Uploaded"+ progress.toInt() + "%...")
-                    }
-        }
-    }
-
-    private fun showFileChooser() {
-        val intent = Intent()
-        intent.type = "image/*"
-        intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent,"SELECT PICTURE"),PICK_IMAGE_REQUEST)
-    }
-
-    private fun startGallery() {
-        val cameraIntent = Intent(Intent.ACTION_GET_CONTENT)
-        cameraIntent.type = "image/*"
-        if (cameraIntent.resolveActivity(this.packageManager) != null) {
-            startActivityForResult(cameraIntent, REQUEST_CODE)
-        }
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == PICK_IMAGE_REQUEST &&
-                resultCode == Activity.RESULT_OK &&
-                data != null && data.data != null) {
-            filePath = data.data
-            try {
-                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filePath)
-                ivSelected.setImageBitmap(bitmap)
-            }catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-
 
     val EXTRA_PHONE = "EXTRA_PHONE"
     @OnClick(R.id.btn_register)
@@ -230,7 +130,7 @@ class NewRegisterActivity : AppCompatActivity(), View.OnClickListener {
 
                 })
 
-                val loginSuccess = Intent(this,LoginSuccessActivity::class.java)
+                val loginSuccess = Intent(this,SelectedActivity::class.java)
                 loginSuccess.putExtra(EXTRA_PHONE,edtPhoneNumber.text.toString())
                 loginSuccess.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
                 startActivity(loginSuccess)
---
+++ app/src/main/java/th/co/todsphol/add/projectone/activity/SelectedActivity.kt
@@ -0,0 +1,108 @@
+package th.co.todsphol.add.projectone.activity
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import th.co.todsphol.add.projectone.R
+import java.io.IOException
+import java.util.*
+
+@Suppress("DEPRECATION")
+class SelectedActivity : AppCompatActivity(), View.OnClickListener {
+
+    @BindView(R.id.tv_show_success) lateinit var tvSuccess : TextView
+    @BindView(R.id.btn_select) lateinit var btnSelected : Button
+    @BindView(R.id.imageView) lateinit var ivSelected : ImageView
+    @BindView(R.id.btn_confirm_register) lateinit var btnConfirm : Button
+    private var filePath : Uri? = null
+    var storage : FirebaseStorage? = null
+    var storageReference : StorageReference? = null
+    val PICK_IMAGE_REQUEST = 1234
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selected)
+        ButterKnife.bind(this)
+        tvSuccess.text = intent.getStringExtra(EXTRA_PHONE)
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage?.reference
+        btnSelected.setOnClickListener(this)
+        btnConfirm.setOnClickListener(this)
+
+    }
+    override fun onClick(p0: View?) {
+        if (p0 == btnSelected) {
+            isselected()
+        } else if (p0 == btnConfirm) {
+            isUploading()
+        }
+    }
+
+    private fun isUploading() {
+        if (filePath != null) {
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Uploading...")
+            progressDialog.show()
+
+            val imageRef = storageReference!!.child("images/User/0968613128/" + UUID.randomUUID().toString())
+            imageRef.putFile(filePath!!)
+                    .addOnSuccessListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext, "File Uploaded", Toast.LENGTH_SHORT).show()
+
+                        val gotoLoginSucIntent = Intent(this, LoginSuccessActivity::class.java)
+                        gotoLoginSucIntent.putExtra("EXTRA_PHONE",tvSuccess.text.toString())
+                        gotoLoginSucIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(gotoLoginSucIntent)
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+
+                    }
+                    .addOnFailureListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnProgressListener {taskSnapShot ->
+                        val progress = 100.0 * taskSnapShot.bytesTransferred/taskSnapShot.totalByteCount
+                        progressDialog.setMessage("Uploaded"+ progress.toInt() + "%...")
+                    }
+        }
+    }
+
+    private fun isselected() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent,"SELECT PICTURE"),PICK_IMAGE_REQUEST)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST &&
+                resultCode == Activity.RESULT_OK &&
+                data != null && data.data != null) {
+            filePath = data.data
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filePath)
+                ivSelected.setImageBitmap(bitmap)
+            }catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    companion object {
+        const val EXTRA_PHONE = "EXTRA_PHONE"
+
+    }
+}
