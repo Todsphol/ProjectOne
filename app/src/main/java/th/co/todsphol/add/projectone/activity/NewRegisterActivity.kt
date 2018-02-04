040aea81aaa67d5138319c2b4831a29dafa694e7
Todsphol <theaddza11@gmail.com>
2018-02-04 23:39:35 +0700

    Clean cide

--- app/src/main/java/th/co/todsphol/add/projectone/activity/NewRegisterActivity.kt
+++ app/src/main/java/th/co/todsphol/add/projectone/activity/NewRegisterActivity.kt
@@ -1,7 +1,6 @@
 package th.co.todsphol.add.projectone.activity
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -15,8 +14,6 @@ import butterknife.ButterKnife
 import com.jaredrummler.materialspinner.MaterialSpinner
 import th.co.todsphol.add.projectone.R
 import android.support.design.widget.Snackbar
-import android.view.View
-import android.view.inputmethod.InputMethod
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import butterknife.OnClick
