20fb78cefb844325f6ee01e3a8fee44d8d9b80c2
Todsphol <theaddza11@gmail.com>
2018-02-15 23:48:52 +0700

    Create new activity showlocation

---
+++ app/src/main/java/th/co/todsphol/add/projectone/activity/ShowLocationActivity.kt
@@ -0,0 +1,12 @@
+package th.co.todsphol.add.projectone.activity
+
+import android.app.Activity
+import android.os.Bundle
+
+class ShowLocationActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_location)
+    }
+}
